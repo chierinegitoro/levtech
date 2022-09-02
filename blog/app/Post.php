0f6087d1ee4db117d3c80a62579448e3f288700b
chierinegitoro <chieri.0417.disney@gmail.com>
2022-09-02 13:28:57 +0900

    2rd step

---
+++ blog/app/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    public function getByLimit(int $limit_count = 10)
+    {
+        return $this->orderBy('updated_at','DESC')->limit($limit_count)->get();
+    }
+    
+    public function getPaginateBylimit(int $limit_count = 5)
+    {
+        return $this->orderBy('updated_at','DESC')->paginate($limit_count);
+    }
+}
+
+?>
